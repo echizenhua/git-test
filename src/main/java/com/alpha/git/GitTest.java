f51003cb88978c9a38c22dc9450446338dfd4679
echizen_hua <echizen_hua@163.com>
2022-08-26 14:52:59 +0800

    first commit

---
+++ src/main/java/com/alpha/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.alpha.git;
+
+/**
+ * @author:echizen_hua
+ * @create:2022-08-26 14:48
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
